6cd5a2b74531b0e60b53f834b908c99e80d8e346
Yaroslav Orlov <ulverforest@gmail.com>
2023-04-06 15:16:58 +0300

    Initial Commit

---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by Yaroslav Orlov on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
