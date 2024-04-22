1f8f99003590f60febe10317c17faee3a8209ca9
Yaroslav Orlov <ulverforest@gmail.com>
2024-04-22 13:18:33 +0300

    create EventListView

--- EventHub/EventsList/EventListCell.swift
+++ EventHub/EventsList/EventListCell.swift
@@ -8,48 +8,78 @@
 import SwiftUI
 
 struct EventListCell: View {
+    let event: Event
+    
     var body: some View {
         HStack(spacing: 18) {
-            SwiftUI.Image("jazz")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 79)
-                .cornerRadius(10)
-                .padding(.leading, 8)
-                .padding(.vertical, 10)
+            let image = event.images[2].url
+            
+            AsyncImage(url: URL(string: image)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 79, height: 92)
+                    .cornerRadius(10)
+                    .padding(.leading, 8)
+                    .padding(.vertical, 10)
+            } placeholder: {
+                ProgressView()
+            }
             
             VStack(alignment: .leading, spacing: 5) {
-                Text("Wed, Apr 28 • 5:30 PM")
+                Text("\(dateFormatter(date: event.dates.start.localDate))")
                     .font(.system(size: 13, weight: .regular))
                     .foregroundColor(.blue)
                 
-                Text("Jo Malone London’s Mother’s Day Presents")
+                Text(event.name)
                     .font(.system(size: 15, weight: .medium))
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.5)
                 
                 HStack(spacing: 2) {
                     SwiftUI.Image("location")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 14, height: 14)
-                    Text("Radius Gallery • Santa Cruz, CA")
+                    Text(event.embedded.venues.map {$0.name ?? ""}.joined(separator: "•"))
                         .font(.system(size: 13, weight: .regular))
                         .foregroundColor(.gray)
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
                 }
             }
             .padding(.trailing, 8)
             .padding(.vertical, 10)
+            Spacer()
         }
         .background(.white)
-        .opacity(0.5)
         .cornerRadius(16)
         .frame(maxWidth: UIScreen.main.bounds.width - 48)
-        .frame(maxHeight: 106)
     }
 }
 
-struct EventListCell_Previews: PreviewProvider {
-    static var previews: some View {
-        EventListCell()
-            .previewLayout(.sizeThatFits)
+extension EventListCell {
+    private func dateFormatter(date: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        if let date = formatter.date(from: date) {
+            formatter.dateFormat = "d MMMM, yyyy"
+            return formatter.string(from: date)
+        } else {
+            return "Date Unknown"
+        }
+    }
+    
+    private func timeFormatter(date: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        formatter.timeZone = TimeZone.current
+        
+        if let fullDate = formatter.date(from: date) {
+            formatter.dateFormat = "HH:mm"
+            return formatter.string(from: fullDate)
+        } else {
+            return "Time Unknown"
+        }
     }
 }
---
+++ EventHub/EventsList/EventListView.swift
@@ -0,0 +1,31 @@
+//
+//  EventListView.swift
+//  EventHub
+//
+//  Created by Yaroslav Orlov on 22.04.2024.
+//
+
+import SwiftUI
+
+struct EventListView: View {
+    @StateObject var eventViewModel = EventsViewModel()
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(eventViewModel.events, id: \.self) { event in
+                        EventListCell(event: event)
+                    }
+                }
+            }
+        }
+        .background(Colors.lightBg)
+    }
+}
+
+struct EventListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventListView()
+    }
+}
