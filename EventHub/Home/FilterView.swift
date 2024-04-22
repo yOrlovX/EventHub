7eb123a0f4bcad298a67232740d79a7d032aaadc
Yaroslav Orlov <ulverforest@gmail.com>
2024-04-22 17:25:42 +0300

    add location button container on filter view

--- EventHub/Home/FilterView.swift
+++ EventHub/Home/FilterView.swift
@@ -20,13 +20,45 @@ struct FilterView: View {
             .font(.system(size: 16, weight: .medium))
             
             Section("Location") {
-                
+                changeLocationButton
             }
             .font(.system(size: 16, weight: .medium))
         }
         .padding(.horizontal, 20)
         
         .navigationTitle("Filter")
+    }
+}
+
+extension FilterView {
+    private var changeLocationButton: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(Colors.lightBlue)
+                .opacity(0.5)
+                .frame(width: 30, height: 30)
+                .overlay {
+                    SwiftUI.Image("location")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Colors.primaryBlue)
+                        .scaledToFit()
+                        .frame(width: 15, height: 15)
+                }
+                .padding(.vertical, 10)
+                .padding(.leading, 10)
+            Text("New Yourk, USA")
+                .font(.system(size: 16, weight: .medium))
+            Spacer()
+            SwiftUI.Image(systemName: "chevron.right")
+                .foregroundColor(Colors.primaryBlue)
+                .padding(.trailing, 10)
+        }
+        .frame(maxWidth: UIScreen.main.bounds.width - 40)
+        .overlay {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.gray, lineWidth: 1)
+        }
     }
 }
 
