e3ec6e48b8a76b3f8c7a94e655484e009d114d31
Yaroslav Orlov <ulverforest@gmail.com>
2024-04-24 16:16:58 +0300

    add paging event for mapView

--- EventHub/Map/MapView.swift
+++ EventHub/Map/MapView.swift
@@ -10,13 +10,41 @@ import MapKit
 
 struct MapView: View {
     @StateObject var viewModel = EventsViewModel()
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.0902, longitude: 95.7129), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.052235, longitude: -118.243683), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    @State private var currentPage = 0
+    
+    init() {
+        UIScrollView.appearance().isPagingEnabled = true
+    }
     
     var body: some View {
-        Map(coordinateRegion: $region, annotationItems: viewModel.places) {
-            MapPin(coordinate: $0.coordinate)
+        VStack {
+            Map(coordinateRegion: $region, showsUserLocation: true, annotationItems: viewModel.places) {
+                MapPin(coordinate: $0.coordinate)
+            }
+            .ignoresSafeArea(.all)
+            .overlay {
+                VStack {
+                    Text("Place for search bar...")
+                    Spacer()
+                    TabView(selection: $currentPage) {
+                        ForEach(viewModel.events.indices, id: \.self) { index in
+                            EventListCell(event: viewModel.events[index])
+                                .tag(index)
+                        }
+                    }
+                    .frame(maxHeight: 106)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .onChange(of: currentPage) { newPage in
+                        print("new page is: \(newPage)")
+                        let selectedEvent = viewModel.places[newPage]
+                        region.center = CLLocationCoordinate2D(latitude: selectedEvent.coordinate.latitude, longitude: selectedEvent.coordinate.longitude)
+                        print("\(selectedEvent.name)")
+                    }
+                }
+            }
+            
         }
-        .ignoresSafeArea(.all)
     }
 }
 
